b137a6791c3b93632d44afbbb7ff36070b2d2f7c
SC-201710111528\Administrator <nining0924@163.com>
2018-08-07 10:06:54 +0800

    集成swagger

---
+++ src/main/java/com/hbtzfx/config/Swagger2.java
@@ -0,0 +1,43 @@
+package com.hbtzfx.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * 对外开放的接口 接口文档生成
+ * Created by lining on 2018/06/14.
+ */
+@EnableSwagger2
+@Configuration
+public class Swagger2 {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                //为controller包路径
+                .apis(RequestHandlerSelectors.basePackage("com.hbtzfx.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+    //构建 api文档的详细信息函数
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                //页面标题
+                .title("Spring Boot使用 Swagger2 构建RestFul API")
+                //创建人
+                .contact("你永远的宁锅锅")
+                //版本号
+                .version("1.0")
+                //描述
+                .description("定时调度接口文档")
+                .build();
+    }
+}
